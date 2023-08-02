af2fb565c41fffecb9c5cabb11deffcd5ef88317
Teona Zavrashvili <101217572+TeonaZav@users.noreply.github.com>
2023-08-02 06:37:49 +0400

    feat: add fake api, fetch data and add useReducer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+
+const initialState = {
+  questions: [],
+  //"loading", "error", "ready", "active", "finished"
+  status: "loading",
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Unknown action!");
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const { questions, status } = state;
+
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
     <div className="App">
-      <DateCounter />
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "ready" && <StartScreen />}
+        {status === "error" && <Error />}
+      </Main>
     </div>
   );
 }
---
+++ src/StartScreen.js
@@ -0,0 +1,11 @@
+function StartScreen() {
+  return (
+    <div className="start">
+      <h2>Welcome to the React Quiz!</h2>
+      <h3>X question to test youe React mastery</h3>
+      <button>Start Quiz</button>
+    </div>
+  );
+}
+
+export default StartScreen;
