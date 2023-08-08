64e3c92bd4bf2f36accde27b1f1e8c552eed6d68
Teona Zavrashvili <101217572+TeonaZav@users.noreply.github.com>
2023-08-08 09:21:32 +0400

    feat: add points

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ const initialState = {
   status: "loading",
   index: 0,
   answer: null,
+  points: 0,
 };
 function reducer(state, action) {
   switch (action.type) {
@@ -22,7 +23,15 @@ function reducer(state, action) {
     case "start":
       return { ...state, status: "active" };
     case "newAnswer":
-      return { ...state, answer: action.payload };
+      const question = state.questions.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
     default:
       throw new Error("Unknown action!");
   }
